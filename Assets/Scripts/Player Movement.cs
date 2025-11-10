a0ebc10caf1d0388905c1ac31814551344676ebc
jacobperez47 <77548177+jacobperez47@users.noreply.github.com>
2025-11-09 23:32:02 -0800

    added a dash

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -11,6 +11,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     
     private Vector2 movement;
+    [Header("Dashing")]
+    [SerializeField] private float dashSpeed = 20.0f;
+    [SerializeField] private float dashTime = 0.1f;
+    [SerializeField] private float dashCooldown = 0.5f;
+    private Vector2 _dashDir;
+    private float dashTimer;
+    private bool canDash = true;
+    private bool isDashing;
     
     public CollectableManager collectableManager;
     // Start is called before the first frame update
@@ -22,13 +30,46 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      
+    }
+
+    void FixedUpdate()
+    {
+        if (isDashing)
+        {
+                rb.velocity = _dashDir.normalized * dashSpeed;
+            
+                return;
+        }
         rb.velocity = movement * moveSpeed;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        movement = context.ReadValue<Vector2>();
-      
+        if (!isDashing)
+        {
+            movement = context.ReadValue<Vector2>();
+        }
+    }
+    
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (canDash && context.performed) 
+        {
+        print("Dash");
+            
+            isDashing = true;
+            canDash = false;
+            _dashDir = movement;
+            if (_dashDir == Vector2.zero)
+            {
+                _dashDir = new Vector2(transform.localScale.x > 0 ? 1 : -1, 0);
+            }
+            StartCoroutine(StopDashing());
+            StartCoroutine(DashCooldown());
+        }
+
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -39,5 +80,18 @@ public class PlayerMovement : MonoBehaviour
             other.gameObject.SetActive(false);
             collectableManager.count++;
         }
+    }
+
+    private IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(dashTime);
+        print("Stop Dashing");
+        isDashing = false;
+    }
+    
+    private IEnumerator DashCooldown()
+    {
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
