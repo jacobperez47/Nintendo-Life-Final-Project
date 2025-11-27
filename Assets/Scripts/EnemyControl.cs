8645a84f73ea0a48e7a39c05040808734528f749
Hailey Campbell <Haileybee2004@gmail.com>
2025-11-26 16:52:50 -0800

    Create EnemyControl.cs
    Used to control the patrolling enemies.

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,47 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    public float speed = 2f;
+    private Rigidbody rb;
+    private Transform currentPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentPoint = pointB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.linearVelocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-speed, 0);
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
